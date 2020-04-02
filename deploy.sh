d252309cff96ad0e3bf89b00e854269173ee9ebe
Eduardo Martins <eduardomartins@Eduardos-MacBook-Pro.local>
2020-04-02 20:05:27 +0100

    Add heroku

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -x
+
+git remote |grep -e \s*heroku$
+
+if [ "$?" != "0" ]
+then
+  npx heroku create --buildpack https://github.com/chibat/heroku-buildpack-deno.git
+fi
+
+git push heroku master
